b89f77d0a7a8606f8ea57c2ad2ad7d7616d0c509
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-11-27 20:56:51 +0100

    Enable highlights

--- queries/angular/highlights.scm
+++ queries/angular/highlights.scm
@@ -1,74 +1,74 @@
-; (identifier) @variable
-; (pipe_sequence "|" @operator)
-; (string) @string
-; (number) @number
-; (pipe_call
-;   name: (identifier) @function)
-; (pipe_call
-;   arguments: (pipe_arguments
-;                (identifier) @parameter))
-;
-; (structural_assignment
-;   operator: (identifier) @keyword)
-;
-; (member_expression
-;   property: (identifier) @property)
-;
-; (call_expression
-;   function: (identifier) @function)
-;
-; (call_expression
-;   function: ((identifier) @function.builtin
-;     (#eq? @function.builtin "$any")))
-;
+(identifier) @variable
+(pipe_sequence "|" @operator)
+(string) @string
+(number) @number
+(pipe_call
+  name: (identifier) @function)
+(pipe_call
+  arguments: (pipe_arguments
+               (identifier) @parameter))
+
+(structural_assignment
+  operator: (identifier) @keyword)
+
+(member_expression
+  property: (identifier) @property)
+
+(call_expression
+  function: (identifier) @function)
+
+(call_expression
+  function: ((identifier) @function.builtin
+    (#eq? @function.builtin "$any")))
+
 ; (annotation) @punctuation.special
 ;
-; [
-;   (control_flow_keyword)
-;   "let"
-; ] @keyword
-;
-; (special_block_keyword) @type
-;
-; [
-; "("
-; ")"
-; "["
-; "]"
-; "{"
-; "}"
-; ] @punctuation.bracket
-;
-; [
-; ";"
-; "."
-; ","
-; "?."
-; ] @punctuation.delimiter
-;
-; ((identifier) @boolean
-;   (#vim-match? @boolean "^(true|false)$"))
-; ((identifier) @variable.builtin
-;   (#vim-match? @variable.builtin "^(this|\$event|null)$"))
-;
-;
-; [
-;   "-"
-;   "&&"
-;   "+"
-;   "<"
-;   "<="
-;   "="
-;   "=="
-;   "==="
-;   "!="
-;   "!=="
-;   ">"
-;   ">="
-;   "*"
-;   "/"
-;   "||"
-;   "%"
-; ] @operator
-;
-; (ternary_operator) @conditional.ternary
+[
+  (control_flow_keyword)
+  "let"
+] @keyword
+
+(special_block_keyword) @type
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+  ";"
+  "."
+  ","
+  "?."
+] @punctuation.delimiter
+
+((identifier) @boolean
+  (#vim-match? @boolean "^(true|false)$"))
+((identifier) @variable.builtin
+  (#vim-match? @variable.builtin "^(this|\$event|null)$"))
+
+
+[
+  "-"
+  "&&"
+  "+"
+  "<"
+  "<="
+  "="
+  "=="
+  "==="
+  "!="
+  "!=="
+  ">"
+  ">="
+  "*"
+  "/"
+  "||"
+  "%"
+] @operator
+
+(ternary_operator) @conditional.ternary
