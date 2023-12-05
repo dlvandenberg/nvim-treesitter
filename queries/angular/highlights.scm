38cd1f4a20a39c02873afcbed37c75cbbd4d94ad
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-12-05 15:27:45 +0100

    Disable highlights

--- queries/angular/highlights.scm
+++ queries/angular/highlights.scm
@@ -1,79 +1,79 @@
 ; inherits: html
 
-(identifier) @variable
-(pipe_sequence "|" @operator)
-(string) @string
-(number) @number
-(pipe_call
-  name: (identifier) @function)
-(pipe_call
-  arguments: (pipe_arguments
-               (identifier) @parameter))
-
-(structural_assignment
-  operator: (identifier) @keyword)
-
-(member_expression
-  property: (identifier) @property)
-
-(call_expression
-  function: (identifier) @function)
-
-(call_expression
-  function: ((identifier) @function.builtin
-    (#eq? @function.builtin "$any")))
-
-(if_statement "\@" @type)
-
-[
-  (control_keyword)
-  "let"
-] @keyword
-
-(special_keyword) @type
-
-[
-  "("
-  ")"
-  "["
-  "]"
-  "{"
-  "}"
-] @punctuation.bracket
-
-[
-  ";"
-  "."
-  ","
-  "?."
-] @punctuation.delimiter
-
-((identifier) @boolean
-  (#any-of? @boolean "true" "false"))
-((identifier) @variable.builtin
-  (#any-of? @variable.builtin "this" "\$event" "null"))
-
-
-[
-  "-"
-  "&&"
-  "+"
-  "<"
-  "<="
-  "="
-  "=="
-  "==="
-  "!="
-  "!=="
-  ">"
-  ">="
-  "*"
-  "/"
-  "||"
-  "%"
-] @operator
-
-[
- (ternary_operator)
- (conditional_operator)
-] @conditional.ternary
+; (identifier) @variable
+; (pipe_sequence "|" @operator)
+; (string) @string
+; (number) @number
+; (pipe_call
+;   name: (identifier) @function)
+; (pipe_call
+;   arguments: (pipe_arguments
+;                (identifier) @parameter))
+;
+; (structural_assignment
+;   operator: (identifier) @keyword)
+;
+; (member_expression
+;   property: (identifier) @property)
+;
+; (call_expression
+;   function: (identifier) @function)
+;
+; (call_expression
+;   function: ((identifier) @function.builtin
+;     (#eq? @function.builtin "$any")))
+;
+; (if_statement "\@" @type)
+;
+; [
+;   (control_keyword)
+;   "let"
+; ] @keyword
+;
+; (special_keyword) @type
+;
+; [
+;   "("
+;   ")"
+;   "["
+;   "]"
+;   "{"
+;   "}"
+; ] @punctuation.bracket
+;
+; [
+;   ";"
+;   "."
+;   ","
+;   "?."
+; ] @punctuation.delimiter
+;
+; ((identifier) @boolean
+;   (#any-of? @boolean "true" "false"))
+; ((identifier) @variable.builtin
+;   (#any-of? @variable.builtin "this" "\$event" "null"))
+;
+;
+; [
+;   "-"
+;   "&&"
+;   "+"
+;   "<"
+;   "<="
+;   "="
+;   "=="
+;   "==="
+;   "!="
+;   "!=="
+;   ">"
+;   ">="
+;   "*"
+;   "/"
+;   "||"
+;   "%"
+; ] @operator
+;
+; [
+;  (ternary_operator)
+;  (conditional_operator)
+; ] @conditional.ternary
