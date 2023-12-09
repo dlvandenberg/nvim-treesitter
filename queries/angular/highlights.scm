3d8e0d83b74b82a602d8f8d5cdb8eef4a27801b3
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-12-09 15:25:46 +0100

    Updated parser revision that fixes context expressions. Added highlights for angular

--- queries/angular/highlights.scm
+++ queries/angular/highlights.scm
@@ -1,37 +1,43 @@
 ; inherits: html_tags
 
-; (identifier) @variable
-; (pipe_sequence "|" @operator)
-; (string) @string
-; (number) @number
-; (pipe_call
-;   name: (identifier) @function)
-; (pipe_call
-;   arguments: (pipe_arguments
-;                (identifier) @parameter))
-;
-; (structural_assignment
-;   operator: (identifier) @keyword)
-;
-; (member_expression
-;   property: (identifier) @property)
-;
-; (call_expression
-;   function: (identifier) @function)
-;
-; (call_expression
-;   function: ((identifier) @function.builtin
-;     (#eq? @function.builtin "$any")))
+(identifier) @variable
+(pipe_operator) @operator
+(string) @string
+(number) @number
+ (pipe_call
+   name: (identifier) @function)
+ (pipe_call
+   arguments: (pipe_arguments
+                (identifier) @parameter))
+
+(structural_assignment
+  operator: (identifier) @keyword)
+
+(member_expression
+  property: (identifier) @property)
+
+(call_expression
+  function: (identifier) @function)
+
+(call_expression
+  function: ((identifier) @function.builtin
+    (#eq? @function.builtin "$any")))
+
+(control_keyword) @keyword
+(special_keyword) @type
+
+((identifier) @boolean
+  (#any-of? @boolean "true" "false"))
+((identifier) @variable.builtin
+  (#any-of? @variable.builtin "this" "\$event" "null"))
+
+[
+ (ternary_operator)
+ (conditional_operator)
+] @conditional.ternary
+
 ;
 ; (if_statement "\@" @type)
-;
-; [
-;   (control_keyword)
-;   "let"
-; ] @keyword
-;
-; (special_keyword) @type
-;
 ; [
 ;   "("
 ;   ")"
@@ -47,11 +53,6 @@
 ;   ","
 ;   "?."
 ; ] @punctuation.delimiter
-;
-; ((identifier) @boolean
-;   (#any-of? @boolean "true" "false"))
-; ((identifier) @variable.builtin
-;   (#any-of? @variable.builtin "this" "\$event" "null"))
 ;
 ;
 ; [
@@ -73,7 +74,3 @@
 ;   "%"
 ; ] @operator
 ;
-; [
-;  (ternary_operator)
-;  (conditional_operator)
-; ] @conditional.ternary
