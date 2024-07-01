81c3c1df18e58a059dc7e7145d6b82dbf9bdcdce
Dennis van den Berg <dennis.vandenberg@nedap.com>
2024-07-01 16:03:59 +0200

    test if statement

--- queries/angular/indents.scm
+++ queries/angular/indents.scm
@@ -1,29 +1,42 @@
 ; inherits: html_tags
 
+; [
+;   (defer_start_expression)
+;   (for_start_expression)
+;   (if_start_expression)
+;   (switch_start_expression)
+;   (case_expression)
+;   (default_expression)
+; ] @indent.begin
+;
+; [
+;  (placeholder_expression)
+;  (loading_expression)
+;  (error_expression)
+;  (empty_expression)
+;  (else_if_expression)
+;  (else_expression)
+; ] @indent.branch
+;
+; [
+;  (defer_end_expression)
+;  (for_end_expression)
+;  (if_end_expression)
+;  (case_end_expression)
+;  (default_end_expression)
+;  (switch_end_expression)
+; ] @indent.end
+;
+
 [
-  (defer_start_expression)
-  (for_start_expression)
-  (if_start_expression)
-  (switch_start_expression)
-  (case_expression)
-  (default_expression)
+ (if_statement)
 ] @indent.begin
 
 [
- (placeholder_expression)
- (loading_expression)
- (error_expression)
- (empty_expression)
- (else_if_expression)
- (else_expression)
+ (else_if_statement)
+ (else_statement)
 ] @indent.branch
 
 [
- (defer_end_expression)
- (for_end_expression)
  (if_end_expression)
- (case_end_expression)
- (default_end_expression)
- (switch_end_expression)
 ] @indent.end
-
